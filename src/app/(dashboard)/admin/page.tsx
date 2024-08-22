166e2fb2ab8cf7076d8b4d5b41508c32f468b33d
Abul Kalam Azad <me.azad99@gmail.com>
2024-08-22 22:37:49 +0600

    user cards added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,23 @@
-
+import UserCard from "@/components/UserCard";
 
 export default function AdminPage() {
   return (
-    <h1>Admin page</h1>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Admin Left area */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        
+      </div>
+      {/* Admin Right area */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+       <h3>Admin Right Area</h3>
+      </div>
+    </div>
   );
 }
