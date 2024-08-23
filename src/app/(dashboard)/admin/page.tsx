fd5777247f1d83766c093e3ee56924e89b02074b
Abul Kalam Azad <me.azad99@gmail.com>
2024-08-23 23:37:04 +0600

    Student count chart added

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,32 @@
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
 
 export default function AdminPage() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Admin Left area */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* USER CARDS */}
+        {/* User Cards area */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
+        </div>
+         {/* Middle charts */}
+         <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Student count chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* Attendance chart */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <h2>Attendance chart</h2>
+          </div>
+        </div>
+        {/* Bottom charts*/}
+        <div className="w-full h-[500px]">
+           <h2>Admin Bottom Chart Area</h2>
         </div>
         
       </div>
--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -27,12 +27,12 @@ const data = [
 const CountChart = () => {
   return (
     <div className="bg-white rounded-xl w-full h-full p-4">
-      {/* TITLE */}
+      {/* Count chart header */}
       <div className="flex justify-between items-center">
         <h1 className="text-lg font-semibold">Students</h1>
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
-      {/* CHART */}
+      {/* Chart container */}
       <div className="relative w-full h-[75%]">
         <ResponsiveContainer>
           <RadialBarChart
@@ -54,7 +54,7 @@ const CountChart = () => {
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         />
       </div>
-      {/* BOTTOM */}
+      {/* Count chart footer */}
       <div className="flex justify-center gap-16">
         <div className="flex flex-col gap-1">
           <div className="w-5 h-5 bg-lamaSky rounded-full" />
